a8b79fbc60bda633a8fd3f5981adc22c10c7b061
YohannHERBET <jukaxyyo@gmail.com>
2022-04-01 16:20:41 +0200

    initialization of second components and tests

---
+++ fanfood/src/components/Footer.vue
@@ -0,0 +1,19 @@
+<template>
+ <footer>
+  <h1>FOR THE FOOD Footer</h1>
+ </footer>
+</template>
+
+<script>
+
+
+export default {
+  name: 'FanFood-Footer',
+  components: {
+
+  }
+}
+</script>
+
+<style scoped lang="scss">
+</style>
